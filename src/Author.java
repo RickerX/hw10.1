42f4d0cb404b6578e6ef8d4da67c6eac8dfc63c9
roman <roman.pshenichnikov555@gmail.com>
2022-10-01 23:07:46 +0300

    hw10.1 done

--- src/Author.java
+++ src/Author.java
@@ -1,2 +1,32 @@
+import java.util.Objects;
+
 public class Author {
+    private final String firtsName;
+    private final String lastName;
+    public  Author(String firstname, String lastname) {
+        this.firtsName = firstname;
+        this.lastName = lastname;
+    }
+
+    public String getFirtsName() {
+        return this.firtsName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String toString() {
+        return this.firtsName +" "+ this.lastName;
+    }
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Author c2 = (Author) other;
+        return firtsName.equals(c2.firtsName);
+    }
+    public int hashCode() {
+        return Objects.hash(firtsName);
+    }
 }
--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,42 @@
+
 public class Book {
+    private final Author author;
+    private final String nameOfTheBook;
+    private int theYearOfPublishing;
+    public Book (Author author, String nameOfTheBook, int theYearOfPublishing) {
+        this.author = author;
+        this.nameOfTheBook = nameOfTheBook;
+        this.theYearOfPublishing = theYearOfPublishing;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public String getNameOfTheBook() {
+        return this.nameOfTheBook;
+    }
+
+    public int getTheYearOfPublishing() {
+        return this.theYearOfPublishing;
+    }
+
+    public void setTheYearOfPublishing(int theYearOfPublishing) {
+        this.theYearOfPublishing = theYearOfPublishing;
+    }
+
+    public String toString() {
+        return "Автор " + this.author + ", название книги: " + this.nameOfTheBook + ", год издания: " + this.theYearOfPublishing;
+    }
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Book c2 = (Book) other;
+        return author.equals(c2.author);
+    }
+    public int hashCode() {
+        return java.util.Objects.hash(author);
+    }
+
 }
